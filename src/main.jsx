d1bbab28bec1e35a99388656d16d46924300a6f2
ZL <ray040406@gmail.com>
2023-11-27 23:53:05 +1300

    state hooks / timeout

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+import App from './App'
+
+let counter = 1
+
+setInterval(() => {
+  refresh()
+  counter += 1
+}, 1000)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App counter={counter}/>
 )
+
