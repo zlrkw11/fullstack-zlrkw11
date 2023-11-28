0b699e4dc31d34b0ae1250621ecda38530a76377
ZL <ray040406@gmail.com>
2023-11-28 20:18:01 +1300

    final changes for counter

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App counter={counter}/>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
+//renders the app to the screen. In this case, the number count
