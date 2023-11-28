b280898eb6f77f4242dbf9e2a842ad65cd06ac75
ZL <ray040406@gmail.com>
2023-11-28 23:54:53 +1300

    changed buttons to separate functions (previous)

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,27 @@ import { useState } from 'react'
 const App = () => {
   const [counter, setCounter] = useState(0)
 
-  
+  const increaseByOne = () => setCounter(counter+1)
+  const setToZero = () => setCounter(0)
+
+  {/*Passing state*/}
+  const Display = (props) => {
+    return (
+      <div>{props.counter}</div>
+    )
+  }
 
   return (
     <div>
       <div>{counter}</div>
 
       {/*plus button*/}
-      <button onClick={()=>setCounter(counter+1)}> 
+      <button onClick={increaseByOne}> 
         plus
       </button>
       
       {/*reset button*/}
-      <button onClick={()=>setCounter(0)}>
+      <button onClick={setToZero}>
         zero
       </button>
 
