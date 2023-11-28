90761923f42262f355bf332c172b3a2ed8a9efdf
ZL <ray040406@gmail.com>
2023-11-28 20:37:48 +1300

    plus button + reset button

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,25 @@ import { useState } from 'react'
 const App = () => {
   const [counter, setCounter] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-
-  console.log('rendering...', counter)
   
 
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+      
+      //plus button 
+      <button onClick={()=>setCounter(counter+1)}> 
+        plus
+      </button>
+      
+      //resets button
+      <button onClick={()=>setCounter(0)}>
+        zero
+      </button>
+
+    </div>
+    
+    
   )
 }
 
