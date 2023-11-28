904d3a7a13326b4e5a9eb8c61f124b28386a5b7b
ZL <ray040406@gmail.com>
2023-11-28 20:11:05 +1300

    main.jsx resets to original / app.jsx added counter

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ const App = () => {
     () => setCounter(counter + 1),
     1000
   )
+
+  console.log('rendering...', counter)
   
+
   return (
     <div>{counter}</div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,6 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-let counter = 1
-
-setInterval(() => {
-  refresh()
-  counter += 1
-}, 1000)
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App counter={counter}/>
 )
