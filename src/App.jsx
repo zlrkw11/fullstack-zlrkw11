59be42984c09d711eaae197c95eb9acc961f6246
ZL <ray040406@gmail.com>
2023-12-01 01:11:23 +1300

    adding comments for code running procedure

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ import { useState } from 'react'
 
 const App = () => {
   const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
 
-  const increaseByOne = () => setCounter(counter+1)
-  const decreaseByOne = () => setCounter(counter-1)
-  const setToZero = () => setCounter(0)
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter+1)
+  }
+
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter-1)
+  }
+  
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)  
+    setCounter(0)
+  }
 
   {/*displayer for counter, used as a passing state*/}
   const Display = (props) => {
